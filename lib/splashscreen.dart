4a5631e9c0ddf3cb0eb5881330de09a4406ffd94
shrutikahilale <shrutikahilale8@gmail.com>
2022-11-11 20:23:03 +0530

    Updated splash screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:practice3/home.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -21,8 +22,22 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(color: Colors.grey[200]),
-      child: Center(
-        child: Icon(Icons.timelapse, size: 100,),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(
+            image: AssetImage('assets/app-logo.png'),
+            height: 100,
+          ),
+          Text(
+            'Consistency Tracker',
+            style: GoogleFonts.robotoMono(
+              fontSize: 24.0,
+              color: Colors.cyan,
+              decoration:  TextDecoration.none
+            ),
+          ),
+        ],
       ),
     );
   }
